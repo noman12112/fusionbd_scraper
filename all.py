9782c27314555e87fa7f0c85dedd04ca7b523164
noman12112 <110756922+noman12112@users.noreply.github.com>
2023-09-12 02:03:34 +0600

    Add files via upload
    run this python file via scrapy command
    scrapy runspider all.py -o <name of json file> example.json
    
    and name with respective URL will be save for everyfileurl that spider crawled in fusionbd.com

---
+++ all.py
@@ -0,0 +1,32 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
+import scrapy
+import re
+
+class Fusion(CrawlSpider):
+	name = "fusionspider"
+	start_urls = [ 'http://books.fusionbd.com/index.php?sort=1',
+ 'http://page.fusionbd.com/mp3.html',
+ 'http://radiospecials.fusionbd.com/index.php?sort=1&p=1',
+ 'http://page.fusionbd.com/3gpvideos.html',
+ 'http://mp4videos.fusionbd.com/index.php?dir=320x240_Pixels&sort=1&p=1',
+ 'http://mp4videos.fusionbd.com/index.php?dir=640x480_Pixels&p=1&sort=1',
+ 'http://mp4videos.fusionbd.com/index.php?dir=/1080x720_Pixels-HD&p=1&sort=1',
+ 'http://mtube.fusionbd.com',
+ 'http://newnatok.fusionbd.com/index.php?dir=Natok_And_Telefilms&p=1&sort=1',
+ 'http://movies.fusionbd.com/index.php?p=1&sort=1']
+	rules = (Rule(LinkExtractor(restrict_xpaths='//div[@class="fl odd"]'),callback = "parse",follow=True),)
+	
+	def parse(self,response):
+		ls =[ i.url for i in  LinkExtractor(allow='').extract_links(response)]
+		for i in ls:
+			if re.compile('file').search(i):
+				yield response.follow(i,callback=self.downld)
+
+	def downld(self,response):
+		url = response.urljoin(response.xpath('/html/body/h2[2]/a/@href').get())
+		name = url.split('/')[-1]
+		yield{
+			"url":url,
+			"name":name
+					}
